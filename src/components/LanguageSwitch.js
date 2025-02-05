ff52efd1ea67b662e2c665122f9dd710152f3953
TheNofis <matbey122@gmail.com>
2025-02-05 22:16:12 +0300

    add: idk babosh

--- src/components/LanguageSwitch.js
+++ src/components/LanguageSwitch.js
@@ -1,29 +1,43 @@
 "use client";
 
 import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 import { useContext } from "react";
 import { LanguageContext } from "@/context/LanguageContext";
 
 export default function LanguageSwitch() {
-const { changeLanguage, translations } = useContext(LanguageContext);
+  const { changeLanguage, translations } = useContext(LanguageContext);
 
   return (
-    <div className="absolute bottom-5 left-1/2 -translate-x-1/2">
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button>{translations.changelanguage}</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent slide="top">
-        <DropdownMenuItem onClick={() => changeLanguage("ru")}>🇷🇺 Русский</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("en")}>🇺🇸 English</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("ua")}>🇺🇦 Українська</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("br")}>🇧🇷 Brasileira</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => changeLanguage("be")}>🇧🇾 Беларуская</DropdownMenuItem>
-
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="fixed bottom-5 left-1/2 -translate-x-1/2">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>{translations.changelanguage}</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent slide="top">
+          <DropdownMenuItem onClick={() => changeLanguage("ru")}>
+            🇷🇺 Русский
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => changeLanguage("en")}>
+            🇺🇸 English
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => changeLanguage("ua")}>
+            🇺🇦 Українська
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => changeLanguage("br")}>
+            🇧🇷 Brasileira
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => changeLanguage("be")}>
+            🇧🇾 Беларуская
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
